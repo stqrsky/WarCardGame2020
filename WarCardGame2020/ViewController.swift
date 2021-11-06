26e34f5a2791804906e757efd1cb15f25bcd7c6b
Starsky <starsky.m@live.com>
2021-11-06 14:35:31 +0100

    Coding Logic

--- WarCardGame2020/ViewController.swift
+++ WarCardGame2020/ViewController.swift
@@ -17,20 +17,37 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var rightScoreLabel: UILabel!
     
+    var leftScore = 0
+    var rightScore = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func dealTapped(_ sender: Any) {
-        
+        // Randomize some numbers
         let leftNumber = Int.random(in: 2...14)
-        
         let rightNumber = Int.random(in: 2...14)
         
+        // Update the image views
         leftImageView.image = UIImage(named: "card\(leftNumber)")
-        
         rightImageView.image = UIImage(named: "card\(rightNumber)")
+        
+        // Compare the random numbers
+        if leftNumber > rightNumber {
+            // Left side wins
+            leftScore += 1
+            leftScoreLabel.text = String(leftScore)
+        }
+        else if leftNumber < rightNumber {
+            // Right side wins
+            rightScore += 1            
+            leftScoreLabel.text = String(rightScore)
+        }
+        else {
+            // Tie
+        }
     }
     
     
